a7c84d7d3eb2b7b172912ab3b5a7d2bea35690f0
koo-sangwoo <luckytkddn@gmail.com>
2025-04-14 22:33:41 +0900

    feat : 타임리프의 기본 객체들
    - 3.0부터는 기본 객체중 locale만이 지원하고 있음
    - 들어온 파라미터를 컨트롤러를 거치지 않고 바로 사용할 수 있는 객체, 스프링 bean을 호출하여 내부 함수를 바로 실행하는 등의 편의 객체가 다수 존재함

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,6 +1,10 @@
 package hello.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,8 +48,34 @@ public class BasicController {
         return "basic/variable";
     }
 
+    /**
+     * 스프링 부트 3.0 이상부터는 타임리프에서 #locale을 제외한 #request등의 기본 변수를 지원하지 않는다.
+     * 정 필요하다면 아래 함수와 같이 컨트롤러 인자로 받아와 해당 객체를 모델에 추가해주어야한다.
+     * @param model
+     * @param request
+     * @param response
+     * @param session
+     * @return html view
+     */
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request,
+                               HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+
     @Data
-    static class User{
+    static class User {
         private String username;
         private int age;
 
